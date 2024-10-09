3a494de8642d42be095fab3edd0458caebde9efd
Dr. Cranium <edlomail@wi.rr.com>
2024-10-09 17:08:29 -0500

    I added some comments to various files.

--- JS/script1.js
+++ JS/script1.js
@@ -2,6 +2,8 @@ function opencard() {
   document.getElementById('outside').className = 'open-card';
 }
 
+//this is just testing for Git and GitHub
+
 function closecard() {
   document.getElementById('outside').className = '';
 }
